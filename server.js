9dcea682ec49653cb43d5c1329f2bf77e6ccdfb6
Aakash <caakashct@gmail.com>
2026-04-06 21:04:17 +0530

    update mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const mockUser = require("./Middleware/mockUser");
 const userRoutes = require("./routes/userRoutes");
 const recordRoutes = require("./routes/recordRoutes");
@@ -8,10 +9,11 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(express.json());
-app.use("/auth", authRoutes);     
-app.use("/users", userRoutes);  
-app.use("/records", recordRoutes);   
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/records", recordRoutes);
 app.use("/dashboard", dashboardRoutes);
-mongoose.connect("mongodb://localhost:27017/finance");
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.listen(5000, () => console.log("Server running"));
